0acce4344ff501b2dabc56bb8151abce3079d1a1
SCORSATTO <alissonscorsatto21@gmail.com>
2026-07-01 00:34:34 -0300

    Connect Supabase admin auth

--- supabase/config.js
+++ supabase/config.js
@@ -1,5 +1,5 @@
-window.SCORSATTO_SUPABASE_URL = "";
-window.SCORSATTO_SUPABASE_ANON_KEY = "";
+window.SCORSATTO_SUPABASE_URL = "https://lpsbrlrsgupuiakrgbdg.supabase.co";
+window.SCORSATTO_SUPABASE_ANON_KEY = "sb_publishable_pTYvSF9SUQMMIjGaYYxQCw_hn0RlIyK";
 window.SCORSATTO_ADMIN_EMAILS = [
   "alisson@scorsatto.co",
   "miguel@scorsatto.co"
